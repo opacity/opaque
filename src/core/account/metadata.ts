4debae313880861a721be497cbdeb06a1e07694d
Connor H <connorhay@protonmail.com>
2019-04-26 19:48:01 -0400

    More docs for metadata

--- src/core/account/metadata.ts
+++ src/core/account/metadata.ts
@@ -28,30 +28,53 @@ class AccountPreferences {
   }
 }
 
+/**
+ * a metadata class to describe a file as it relates to the UI
+ */
 class FileEntryMeta {
-  file: string
-  created: number
+	/** the name of the file as shown in the UI */
+	name: string
+	/** the date in `ms` that this file was initially updated */
+	created: number
+	/** if the file should be hidden (this could also be automatically generated within the UI, ie. `.files`) */
   hidden: boolean
-  locked: boolean
-  versions: FileVersion[]
+  /**
+   * if the file is encrypted
+   * (will require password in the UI, may need bytes prefixed to meta to determine whether it was encrypted)
+   */
+	locked: boolean
+	/** versions of the uploaded file (the most recent of which should be the current version of the file) */
+	versions: FileVersion[]
+	/** tags assigned to the file for organization/searching */
   tags: string[]
 
+  /**
+   * create metadata for a file entry in the UI
+   *
+   * @param name - the name of the file as shown in the UI
+   * @param created - the date in `ms` that this file was initially updated
+   * @param hidden - if the file should be hidden (this could also be automatically generated within the UI, ie. `.files`)
+   * @param locked - if the file is encrypted
+	 *   (will require password in the UI, may need bytes prefixed to meta to determine whether it was encrypted)
+   * @param versions - versions of the uploaded file (the most recent of which should be the current version of the file)
+   * @param tags - tags assigned to the file for organization/searching
+   */
   constructor ({
-    file,
+    name,
     created = Date.now(),
     hidden = false,
     locked = false,
     versions = [],
     tags = []
   }: {
-    file: string
+    name: string
     created?: number
     hidden?: boolean
     locked?: boolean
     versions?: FileVersion[]
     tags?: string[]
   }) {
-    this.file = file
+    this.name = name
     this.created = created
     this.hidden = hidden
     this.locked = locked
@@ -60,12 +83,31 @@ class FileEntryMeta {
   }
 }
 
+/**
+ * a metadata class to describe a version of a file as it relates to a filesystem
+ */
 class FileVersion {
-  size: number
-  location: string
-  hash: string
-  modified: number
+	/** size in bytes of the file */
+	size: number
+	/** location on the network of the file */
+	location: string
+	/**
+	 * a hash of the file
+	 * NOTE: probably `sha1`
+	 */
+	hash: string
+	/** the date in `ms` that this version of the file was originally changed */
+	modified: number
 
+  /**
+   * create metadata for a file version
+   *
+   * @param size - size in bytes of the file
+   * @param location - location on the network of the file
+   * @param hash - a hash of the file
+	 *   NOTE: probably `sha1`
+   * @param modified - the date in `ms` that this version of the file was originally changed
+   */
   constructor ({
     size,
     location,
@@ -123,9 +165,9 @@ class FolderMeta {
   name: string
   /** the files included only in the most shallow part of the folder */
   files: FileEntryMeta[]
-  /** when the directory was created (if not created now) */
+  /** when the folder was created (if not created now) in `ms` */
   created: number
-  /** if the folder should be hidden (this could also be automatically generated within the UI) */
+  /** if the folder should be hidden (this could also be automatically generated within the UI, ie. `.folders`) */
   hidden: boolean
   /**
    * if the folder's metadata is encrypted
@@ -140,7 +182,7 @@ class FolderMeta {
    *
    * @param name - a nickname shown on the folder when accessed without adding to account metadata
    * @param files - the files included only in the most shallow part of the folder
-   * @param created - when the directory was created (if not created now)
+   * @param created - when the folder was created (if not created now) in `ms`
    * @param hidden - if the folder should be hidden (this could also be automatically generated within the UI)
    * @param locked - if the folder's metadata is encrypted (will require password in the UI)
    *  NOTE: may need bytes prefixed to meta to determine whether it was encrypted
