54e18594d8a462f0860a813be9c9ce0a5e76cfe7
Edmund Mai <edmundmai@gmail.com>
2019-05-16 14:54:09 -0400

    Fix bug for missing account and missing handle

--- src/account.ts
+++ src/account.ts
@@ -1,16 +1,25 @@
-import { generateMnemonic, entropyToMnemonic, mnemonicToSeedSync, validateMnemonic } from "bip39"
-import HDKey, { fromMasterSeed } from "hdkey"
+import {
+  generateMnemonic,
+  entropyToMnemonic,
+  mnemonicToSeedSync,
+  validateMnemonic,
+} from "bip39";
+import HDKey, { fromMasterSeed } from "hdkey";
 
-import Upload from "./upload"
-import Download from "./download"
-import { EventEmitter } from "events"
-import { pipe } from "./utils/pipe"
-import { hash } from "./core/hashing"
-import { decryptString, encryptString } from "./core/encryption"
-import { FolderMeta, FileEntryMeta, FileVersion } from "./core/account/metadata"
-import { getMetadata, setMetadata } from "./core/requests/metadata"
+import Upload from "./upload";
+import Download from "./download";
+import { EventEmitter } from "events";
+import { pipe } from "./utils/pipe";
+import { hash } from "./core/hashing";
+import { decryptString, encryptString } from "./core/encryption";
+import {
+  FolderMeta,
+  FileEntryMeta,
+  FileVersion,
+} from "./core/account/metadata";
+import { getMetadata, setMetadata } from "./core/requests/metadata";
 
-import { RequireOnlyOne } from "./types/require-only-one"
+import { RequireOnlyOne } from "./types/require-only-one";
 
 /**
  * **_this should never be shared or left in storage_**
@@ -18,10 +27,10 @@ import { RequireOnlyOne } from "./types/require-only-one"
  * a class for representing the account mnemonic
  */
 class Account {
-  private _mnemonic: string
+  private _mnemonic: string;
 
-  get mnemonic () {
-    return this._mnemonic.trim().split(/\s+/g)
+  get mnemonic() {
+    return this._mnemonic.trim().split(/\s+/g);
   }
 
   /**
@@ -29,15 +38,15 @@ class Account {
    *
    * @param mnemonic - the mnemonic to use for the account
    */
-  constructor (mnemonic: string = generateMnemonic()) {
+  constructor(mnemonic: string = generateMnemonic()) {
     if (!validateMnemonic(mnemonic))
-      throw new Error("mnemonic provided was not valid")
+      throw new Error("mnemonic provided was not valid");
 
-    this._mnemonic = mnemonic
+    this._mnemonic = mnemonic;
   }
 
-  get seed () {
-    return mnemonicToSeedSync(this._mnemonic)
+  get seed() {
+    return mnemonicToSeedSync(this._mnemonic);
   }
 }
 
@@ -57,17 +66,26 @@ class MasterHandle extends HDKey {
    *
    * @param account - the account to generate the handle from
    */
-  constructor ({ account, handle }: RequireOnlyOne<{ account: Account, handle: string }, "account" | "handle">) {
-    super()
+  constructor({
+    account,
+    handle,
+  }: RequireOnlyOne<
+    { account: Account; handle: string },
+    "account" | "handle"
+  >) {
+    super();
 
-    if (account.constructor == Account) {
+    if (account && account.constructor == Account) {
       // TODO: fill in path
       // ethereum/EIPs#1775 is very close to ready, it would be better to use it instead
-      Object.assign(this, fromMasterSeed(account.seed).derive("m/43'/60'/1775'/0'/path"))
-    } else if (handle.constructor == String) {
-      this.privateKey = Buffer.from(handle, "hex")
+      Object.assign(
+        this,
+        fromMasterSeed(account.seed).derive("m/43'/60'/1775'/0'/path")
+      );
+    } else if (handle && handle.constructor == String) {
+      this.privateKey = Buffer.from(handle, "hex");
     } else {
-      throw new Error("master handle was not of expected type")
+      throw new Error("master handle was not of expected type");
     }
   }
 
@@ -76,80 +94,86 @@ class MasterHandle extends HDKey {
    *
    * @param path - the string to use as a sub path
    */
-  private generateSubHDKey (path: string): HDKey {
-    return (
-      pipe(Buffer.concat([this.privateKey, Buffer.from(hash(path), "hex")]).toString("hex"))
-        .through(
-          hash,
-          entropyToMnemonic,
-          mnemonicToSeedSync,
-          fromMasterSeed
-        )
-    )
+  private generateSubHDKey(path: string): HDKey {
+    return pipe(
+      Buffer.concat([this.privateKey, Buffer.from(hash(path), "hex")]).toString(
+        "hex"
+      )
+    ).through(hash, entropyToMnemonic, mnemonicToSeedSync, fromMasterSeed);
   }
 
-  uploadFile (dir: string, file: File) {
-    const
-      upload = new Upload(file, this),
-      ee = new EventEmitter()
+  uploadFile(dir: string, file: File) {
+    const upload = new Upload(file, this),
+      ee = new EventEmitter();
 
     upload.on("progress", progress => {
-      ee.emit("progress", progress)
-    })
+      ee.emit("progress", progress);
+    });
 
     upload.on("error", err => {
-      ee.emit("error", err)
-      throw err
-    })
+      ee.emit("error", err);
+      throw err;
+    });
 
     upload.on("finish", async ({ handle }: { handle: string }) => {
-      const
-        folderMeta = await this.getFolderMetadata(dir),
-        oldMetaIndex = folderMeta.files.findIndex(e => e.name == file.name && e.type == "file"),
-        oldMeta = oldMetaIndex !== -1 ? folderMeta.files[oldMetaIndex] as FileEntryMeta : {} as FileEntryMeta,
+      const folderMeta = await this.getFolderMetadata(dir),
+        oldMetaIndex = folderMeta.files.findIndex(
+          e => e.name == file.name && e.type == "file"
+        ),
+        oldMeta =
+          oldMetaIndex !== -1
+            ? (folderMeta.files[oldMetaIndex] as FileEntryMeta)
+            : ({} as FileEntryMeta),
         version = new FileVersion({
           size: file.size,
           handle: handle,
-          modified: file.lastModified
+          modified: file.lastModified,
         }),
         meta = new FileEntryMeta({
           name: file.name,
           created: oldMeta.created,
-          versions: (oldMeta.versions || []).unshift(version) && oldMeta.versions
-        })
+          versions:
+            (oldMeta.versions || []).unshift(version) && oldMeta.versions,
+        });
 
       // metadata existed previously
-      if (oldMetaIndex !== -1)
-        folderMeta.files.splice(oldMetaIndex, 1, meta)
-      else
-        folderMeta.files.unshift(meta)
+      if (oldMetaIndex !== -1) folderMeta.files.splice(oldMetaIndex, 1, meta);
+      else folderMeta.files.unshift(meta);
 
-      const buf = Buffer.from(JSON.stringify(folderMeta))
+      const buf = Buffer.from(JSON.stringify(folderMeta));
 
-      const metaUpload = new Upload(buf, this)
+      const metaUpload = new Upload(buf, this);
 
       metaUpload.on("error", err => {
-        ee.emit("error", err)
-        throw err
-      })
+        ee.emit("error", err);
+        throw err;
+      });
 
       metaUpload.on("finish", ({ handle: metaHandle }: { handle: string }) => {
-        const encryptedHandle = encryptString(this.privateKey.toString("hex"), metaHandle)
+        const encryptedHandle = encryptString(
+          this.privateKey.toString("hex"),
+          metaHandle
+        );
 
         // TODO
-        setMetadata("ENDPOINT", this.getFolderHDKey(dir), this.getFolderLocation(dir), encryptedHandle)
-      })
-    })
+        setMetadata(
+          "ENDPOINT",
+          this.getFolderHDKey(dir),
+          this.getFolderLocation(dir),
+          encryptedHandle
+        );
+      });
+    });
 
-    return ee
+    return ee;
   }
 
   downloadFile = (handle: string) => {
-    return new Download(handle)
-  }
+    return new Download(handle);
+  };
 
-  static getKey (from: HDKey, str: string) {
-    return hash(from.privateKey.toString("hex"), str)
+  static getKey(from: HDKey, str: string) {
+    return hash(from.privateKey.toString("hex"), str);
   }
 
   /**
@@ -157,8 +181,8 @@ class MasterHandle extends HDKey {
    *
    * @param file - the location of the file on the network
    */
-  getFileHDKey (file: string) {
-    return this.generateSubHDKey("file: " + file)
+  getFileHDKey(file: string) {
+    return this.generateSubHDKey("file: " + file);
   }
 
   /**
@@ -166,40 +190,40 @@ class MasterHandle extends HDKey {
    *
    * @param dir - the folder path in the UI
    */
-  getFolderHDKey (dir: string) {
-    return this.generateSubHDKey("folder: " + dir)
+  getFolderHDKey(dir: string) {
+    return this.generateSubHDKey("folder: " + dir);
   }
 
-  getFolderLocation (dir: string) {
-    return hash(this.getFolderHDKey(dir).publicKey.toString("hex"))
+  getFolderLocation(dir: string) {
+    return hash(this.getFolderHDKey(dir).publicKey.toString("hex"));
   }
 
-  async getFolderHandle (dir: string) {
-    const
-      folderKey = this.getFolderHDKey(dir),
+  async getFolderHandle(dir: string) {
+    const folderKey = this.getFolderHDKey(dir),
       location = this.getFolderLocation(dir),
-      key = hash(folderKey.privateKey.toString("hex"))
+      key = hash(folderKey.privateKey.toString("hex"));
 
     // TODO
-    const metaLocation = decryptString(key, await getMetadata("ENDPOINT", folderKey, location) as any, "hex")
+    const metaLocation = decryptString(
+      key,
+      (await getMetadata("ENDPOINT", folderKey, location)) as any,
+      "hex"
+    );
 
-    return metaLocation + MasterHandle.getKey(this, metaLocation)
+    return metaLocation + MasterHandle.getKey(this, metaLocation);
   }
 
-  async getFolderMetadata (dir: string) {
-    const handle = await this.getFolderHandle(dir)
+  async getFolderMetadata(dir: string) {
+    const handle = await this.getFolderHandle(dir);
 
     const meta: FolderMeta = await new Promise((resolve, reject) => {
       new Download(handle)
         .on("finish", text => resolve(JSON.parse(text)))
-        .on("error", reject)
-    })
+        .on("error", reject);
+    });
 
-    return meta
+    return meta;
   }
 }
 
-export {
-  Account,
-  MasterHandle
-}
+export { Account, MasterHandle };
