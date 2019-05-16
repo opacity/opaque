2b3ae5db30f94404ab1bd29793418d7b0f6849bb
Connor H <connorhay@protonmail.com>
2019-05-16 04:00:12 -0400

    Fixes, reorganize, and add types

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,10 +1,10 @@
 import FormDataNode from "form-data";
 import * as EthUtil from "ethereumjs-util";
-import Axios from "axios";
+import HDKey from "hdkey"
 
 const POLYFILL_FORMDATA = typeof FormData === "undefined";
 
-export function getPayload(rawPayload, hdNode, key = "requestBody") {
+export function getPayload(rawPayload, hdNode: HDKey, key = "requestBody") {
   const payload = JSON.stringify(rawPayload);
   const hash = EthUtil.keccak256(payload);
   const signature = hdNode.sign(hash).toString("hex");
@@ -20,7 +20,7 @@ export function getPayload(rawPayload, hdNode, key = "requestBody") {
   return signedPayload;
 }
 
-export function getPayloadFD(rawPayload, extraPayload, hdNode, key = "requestBody") {
+export function getPayloadFD(rawPayload: { [key: string]: any }, extraPayload, hdNode: HDKey, key = "requestBody") {
   // rawPayload.timestamp = Date.now();
 
   const payload = JSON.stringify(rawPayload);
@@ -40,10 +40,11 @@ export function getPayloadFD(rawPayload, extraPayload, hdNode, key = "requestBod
     if(extraPayload) {
       Object.keys(extraPayload).forEach(key => {
         const pl = extraPayload[key]
-        data.append(key, data, {
+
+        data.append(key, pl, {
           filename: key,
           contentType: "application/octet-stream",
-          knownLength: data.length
+          knownLength: pl.length
         });
       });
     }
@@ -66,41 +67,6 @@ export function getPayloadFD(rawPayload, extraPayload, hdNode, key = "requestBod
   }
 }
 
-export async function checkPaymentStatus(endpoint, hdNode) {
-  const payload = {
-    timestamp: Math.floor(Date.now() / 1000)
-  }
-
-  const signedPayload = getPayload(payload, hdNode);
-
-  return Axios.post(endpoint + "/api/v1/account-data", signedPayload);
-}
-
-export async function createAccount(endpoint, hdNode, metadataKey) {
-  const payload = {
-    metadataKey: metadataKey,
-    durationInMonths: 12,
-    storageLimit: 100
-  }
-
-  const signedPayload = getPayload(payload, hdNode);
-
-  return Axios.post(endpoint + "/api/v1/accounts", signedPayload);
-}
-
-// Metadata as hexstring as of right now
-export async function writeMetadata(endpoint, hdNode, metadataKey, metadata) {
-  const timestamp = Math.floor(Date.now() / 1000);
-  const payload = { timestamp, metadata, metadataKey };
-  const signedPayload = getPayload(payload, hdNode);
-
-  return Axios.post("/metadata/set", signedPayload);
-}
-
-export async function getMetadata(endpoint, hdNode, metadataKey) {
-  const timestamp = Math.floor(Date.now() / 1000);
-  const payload = { timestamp, metadataKey };
-  const signedPayload = getPayload(payload, hdNode);
-
-  return Axios.post(endpoint + "/metadata/get", signedPayload);
-}
+export { checkPaymentStatus } from "./requests/checkPaymentStatus";
+export { createAccount } from "./requests/createAccount";
+export { getMetadata, setMetadata } from "./requests/metadata";
