abce4fe24f8c1a3c3177dbdd4334f3afa1fcf19a
Connor H <13987924+CKH4@users.noreply.github.com>
2019-07-12 12:02:10 -0400

    Folder prep

--- pkg/dist-src/core/requests/metadata.js
+++ pkg/dist-src/core/requests/metadata.js
@@ -1,5 +1,17 @@
 import Axios from "axios";
 import { getPayload } from "../request";
+export async function createMetadata(endpoint, hdNode, metadataKey) {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const payload = { timestamp, metadataKey };
+    const signedPayload = getPayload(payload, hdNode);
+    return Axios.post(endpoint + "/api/v1/metadata/create", signedPayload);
+}
+export async function deleteMetadata(endpoint, hdNode, metadataKey) {
+    const timestamp = Math.floor(Date.now() / 1000);
+    const payload = { timestamp, metadataKey };
+    const signedPayload = getPayload(payload, hdNode);
+    return Axios.post(endpoint + "/api/v1/metadata/delete", signedPayload);
+}
 // Metadata as hexstring as of right now
 export async function setMetadata(endpoint, hdNode, metadataKey, metadata) {
     const timestamp = Math.floor(Date.now() / 1000);
